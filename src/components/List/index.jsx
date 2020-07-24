3d09c9e3e70b73489a9947da335ba539d7388e23
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-07-24 11:51:47 +0100

    Added netflix and significa project

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -7,18 +7,27 @@ export default class List extends Component {
   render() {
     return (
       <>
-        <div className='introduction_projects' style={{ textAlign: 'center' }}>
-          <p>Here you can access some of my favorite projects.</p>
-        </div>
         <div className='all_cards'>
           <div className='card_space'>
             <ProjectCard
-              link='https://tilesofherozakin.netlify.app'
-              photo='https://res.cloudinary.com/fguerreir0/image/upload/v1593131017/Personal/Captura_de_ecra%CC%83_2020-06-26_a%CC%80s_01.22.10_phwfvj.png'
-              name='Tiles of Herozakin'
-              description='JavaScript Game'
-              github='https://github.com/FGuerreir0/game-project'
-              tech='HTML / CSS / JavaScript (Dom Manipulation)'
+              link='https://fguerreir0-netflixclone.netlify.app/'
+              photo='https://res.cloudinary.com/fguerreir0/image/upload/v1595422834/Personal/netflix-clone.png'
+              name='Netflix Clone'
+              description='Front-end app'
+              github='https://github.com/FGuerreir0/Netflix-Clone'
+              tech='React / Hooks / CSS'
+              api='TMDb / Youtube / Modal'
+            />
+          </div>
+          <div className='card_space'>
+            <ProjectCard
+              link='https://uniflow-ironhack.herokuapp.com'
+              photo='https://res.cloudinary.com/fguerreir0/image/upload/v1593131494/Personal/Captura_de_ecra%CC%83_2020-06-26_a%CC%80s_01.31.13_dexaxh.png'
+              name='uniflow'
+              description='Social Crowdfunding App'
+              github='https://github.com/FGuerreir0/project3-crowdfunding'
+              tech='MongoDB / Express / React / Node'
+              api='Stripe / Cloudinary / Tailwind'
             />
           </div>
           <div className='card_space'>
@@ -32,15 +41,26 @@ export default class List extends Component {
               api='GoogleApi / Nodemailler / Cloudinary / Bootstrap'
             />
           </div>
+
           <div className='card_space'>
             <ProjectCard
-              link='https://uniflow-ironhack.herokuapp.com'
-              photo='https://res.cloudinary.com/fguerreir0/image/upload/v1593131494/Personal/Captura_de_ecra%CC%83_2020-06-26_a%CC%80s_01.31.13_dexaxh.png'
-              name='uniflow'
-              description='Social Crowdfunding App'
-              github='https://github.com/FGuerreir0/project3-crowdfunding'
-              tech='MongoDB / Express / React / Node'
-              api='Stripe / Cloudinary / Tailwind'
+              link='https://fguerreir0-significa-clone-2020.netlify.app/'
+              photo='https://res.cloudinary.com/fguerreir0/image/upload/v1595422834/Personal/significa.png'
+              name='Significa.co Clone'
+              description='Bootstrap Clone'
+              github='https://github.com/FGuerreir0/lab-bootstrap-cloning-template'
+              tech='HTML / CSS / Bootstrap'
+            />
+          </div>
+
+          <div className='card_space'>
+            <ProjectCard
+              link='https://tilesofherozakin.netlify.app'
+              photo='https://res.cloudinary.com/fguerreir0/image/upload/v1593131017/Personal/Captura_de_ecra%CC%83_2020-06-26_a%CC%80s_01.22.10_phwfvj.png'
+              name='Tiles of Herozakin'
+              description='JavaScript Game'
+              github='https://github.com/FGuerreir0/game-project'
+              tech='HTML / CSS / JavaScript (Dom Manipulation)'
             />
           </div>
         </div>
